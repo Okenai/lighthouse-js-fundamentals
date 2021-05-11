ccb5d1c4beba93f07d19a7803bfc79b71c4441ca
Lana <kassembayeva@gmail.com>
2021-05-11 13:43:34 +0000

    for loop modified into while

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
-for (let i = 0; i < packingList.length; i++) {
-console.log(packingList[i]);
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
-
 
